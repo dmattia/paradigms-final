34358150c2b9fd82353010d46cd9189cbb3f7fb8
dmattia <dmattia@nd.edu>
2016-04-30 23:14:51 -0400

    Refactor server code slightly

--- main.py
+++ main.py
@@ -21,12 +21,6 @@ class GameSpace:
 	def main(self):
 		pygame.init()
 		self.size = self.width, self.height = 640, 480
-		"""
-		self.black = 0, 0, 0
-		self.white = 255, 255, 255
-		self.red = 255, 0, 0
-		self.screen = pygame.display.set_mode(self.size)
-		"""
 		self.speed = 4.0
 
 		self.player1 = Player(40, self)
@@ -35,37 +29,15 @@ class GameSpace:
 
 		self.lc = LoopingCall(self.game_loop_iterate)
 		self.lc.start(1.0/60)	
-		#self.lc.stop()
 
 	def game_loop_iterate(self):
-		####
-		# Check for exit
-		####
-		"""
-		for event in pygame.event.get():
-			if event.type == QUIT:
-				pygame.quit()
-				os._exit(0)
-		"""
-
-		####
-		# check current keydown
-		####
-		"""
-		keys_pressed = pygame.key.get_pressed()
-		if keys_pressed[pygame.K_UP]:
-			self.player1.moveUp()
-		if keys_pressed[pygame.K_DOWN]:
-			self.player1.moveDown()
-		"""
-
 		####
 		# Check for collision
 		####
 		if self.ball.y_pos <= self.ball.radius or self.ball.y_pos >= self.height - self.ball.radius:
 			self.ball.hitWall()
 		elif self.ball.x_pos - self.ball.radius <= self.player1.x_pos + (self.player1.width / 2.0):
-			# ball is close to the right side of player 1
+			# ball is close to the left side
 			if self.ball.y_pos >= self.player1.y_pos - (self.player1.height / 2.0) \
 			   and self.ball.y_pos <= self.player1.y_pos + (self.player1.height / 2.0):
 				 self.ball.hitPlayer(self.player1.y_pos, self.player1.height)
@@ -73,7 +45,7 @@ class GameSpace:
 				self.player2.score += 1
 				self.ball = Ball(self.speed)
 		elif self.ball.x_pos + self.ball.radius >= self.player2.x_pos - (self.player2.width / 2.0):
-			# ball is close to the right side of player 2
+			# ball is close to the right side
 			if self.ball.y_pos >= self.player2.y_pos - (self.player2.height / 2.0) \
 			   and self.ball.y_pos <= self.player2.y_pos + (self.player2.height / 2.0):
 				 self.ball.hitPlayer(self.player2.y_pos, self.player2.height)
@@ -89,33 +61,8 @@ class GameSpace:
 		self.ball.tick()
 
 		global p1Server, p2Server
-		if p1Server.transport:
-			p1Server.transport.write(self.to_json() + "?")
-		else:
-			print "p1 server has no transport yet"
-		if p2Server.transport:
-			p2Server.transport.write(self.to_json() + "?")
-		else:
-			print "p2 server has no transport yet"
-
-		"""
-		####
-		# Draw objects
-		####
-		self.screen.fill(self.black)
-		pygame.draw.rect(self.screen, self.white, self.player1.getRect())
-		pygame.draw.rect(self.screen, self.white, self.player2.getRect())
-		pygame.draw.circle(self.screen, self.red, self.ball.getPos(), self.ball.radius)
-
-		####
-		# Draw Score
-		####
-		myfont = pygame.font.SysFont("monospace", 42)
-		score_label = myfont.render(str(self.player1.score) + " | " + str(self.player2.score), 1, self.white)
-		self.screen.blit(score_label, (260, 20))
-
-		pygame.display.flip()
-		"""
+		p1Server.transport.write(self.to_json() + "?")
+		p2Server.transport.write(self.to_json() + "?")
 
 	def to_json(self):
 		""" Creates a dictionary @gsData that represents the gamestate.
